4ce83fd0a4ec34eb692cec7b7cf2e72e715dcfb2
Aaron Schraufnagel <aaronts86@gmail.com>
2018-03-20 18:02:13 -0700

    Fixing API

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -12,22 +12,22 @@ export class AppService {
   constructor(private http: HttpClient) {}
 
   getAllCats(): Observable<Cat[]> {
-    return this.http.get<Cat[]>('http://localhost:8000/api/cats');
+    return this.http.get<Cat[]>('/api/cats');
   }
 
   getCat(name: string): Observable<Cat> {
-    return this.http.get<Cat>('http://localhost:800/api/cats/' + name);
+    return this.http.get<Cat>('/api/cats/' + name);
   }
 
   insertCat(cat: Cat): Observable<Cat> {
-    return this.http.post<Cat>('http://localhost:8000/api/cats/', cat);
+    return this.http.post<Cat>('/api/cats/', cat);
   }
 
   updateCat(cat: Cat): Observable<void> {
-    return this.http.put<void>('http://localhost:8000/api/cats/' + cat.name, cat);
+    return this.http.put<void>('/api/cats/' + cat.name, cat);
   }
 
   deleteCat(name: string) {
-    return this.http.delete('http://localhost:8000/api/cats/' + name);
+    return this.http.delete('/api/cats/' + name);
   }
 }
